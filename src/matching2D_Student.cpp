a5b9557ebd0ea20e15bca13984cf17b6d7708a6b
Hiep Truong <truongconghiep@gmail.com>
2019-08-24 20:48:13 +0200

    added harris detector

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -154,32 +154,88 @@ void detKeypointsShiTomasi(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool b
     }
 }
 
-void detKeypointsFast(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis)
+void detKeypointsHarris(vector<cv::KeyPoint> &keypoints, cv::Mat &img)
+{
+    cv::cvtColor(img, img, cv::COLOR_BGR2GRAY); // convert to grayscale
+
+    // Detector parameters
+    int blockSize = 2;     // for every pixel, a blockSize × blockSize neighborhood is considered
+    int apertureSize = 3;  // aperture parameter for Sobel operator (must be odd)
+    int minResponse = 100; // minimum value for a corner in the 8bit scaled response matrix
+    double k = 0.04;       // Harris parameter (see equation for details)
+
+    // Detect Harris corners and normalize output
+    cv::Mat dst, dst_norm, dst_norm_scaled;
+    dst = cv::Mat::zeros(img.size(), CV_32FC1);
+    cv::cornerHarris(img, dst, blockSize, apertureSize, k, cv::BORDER_DEFAULT);
+    cv::normalize(dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
+    cv::convertScaleAbs(dst_norm, dst_norm_scaled);
+
+    // Look for prominent corners and instantiate keypoints
+    double maxOverlap = 0.0; // max. permissible overlap between two features in %, used during non-maxima suppression
+    for (size_t j = 0; j < dst_norm.rows; j++)
+    {
+        for (size_t i = 0; i < dst_norm.cols; i++)
+        {
+            int response = (int)dst_norm.at<float>(j, i);
+            if (response > minResponse)
+            { // only store points above a threshold
+
+                cv::KeyPoint newKeyPoint;
+                newKeyPoint.pt = cv::Point2f(i, j);
+                newKeyPoint.size = 2 * apertureSize;
+                newKeyPoint.response = response;
+
+                // perform non-maximum suppression (NMS) in local neighbourhood around new key point
+                bool bOverlap = false;
+                for (auto it = keypoints.begin(); it != keypoints.end(); ++it)
+                {
+                    double kptOverlap = cv::KeyPoint::overlap(newKeyPoint, *it);
+                    if (kptOverlap > maxOverlap)
+                    {
+                        bOverlap = true;
+                        if (newKeyPoint.response > (*it).response)
+                        {                      // if overlap is >t AND response is higher for new kpt
+                            *it = newKeyPoint; // replace old key point with new one
+                            break;             // quit loop over keypoints
+                        }
+                    }
+                }
+                if (!bOverlap)
+                {                                     // only add new key point if no overlap has been found in previous NMS
+                    keypoints.push_back(newKeyPoint); // store new keypoint in dynamic list
+                }
+            }
+        } // eof loop over cols
+    }     // eof loop over rows
+}
+
+void detKeypointsFast(vector<cv::KeyPoint> &keypoints, cv::Mat &img)
 {
     Ptr<FastFeatureDetector> detector = FastFeatureDetector::create(10, true);
     detector->detect(img, keypoints);
 }
 
-void detKeypointsBrisk(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis)
+void detKeypointsBrisk(vector<cv::KeyPoint> &keypoints, cv::Mat &img)
 {
     Mat desc;
     Ptr<BRISK> brisk = BRISK::create();
     brisk->detect(img, keypoints);
 }
 
-void detKeypointsAkaze(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis)
+void detKeypointsAkaze(vector<cv::KeyPoint> &keypoints, cv::Mat &img)
 {
     Ptr<AKAZE> akaze = AKAZE::create();
     akaze->detect(img, keypoints);
 }
 
-void detKeypointsSift(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis)
+void detKeypointsSift(vector<cv::KeyPoint> &keypoints, cv::Mat &img)
 {
     Ptr<Feature2D> sift = SIFT::create();
     sift->detect(img, keypoints);
 }
 
-double detKeypointsModern(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, std::string detectorType, bool bVis)
+double detKeypointsModern(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, std::string detectorType)
 {
     double t = (double)cv::getTickCount();
     if(detectorType.compare("SHITOMASI") == 0)
@@ -188,7 +244,7 @@ double detKeypointsModern(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, st
     }
     else if (detectorType.compare("HARRIS") == 0)
     {
-
+        detKeypointsHarris(keypoints, img);
     }
     else if (detectorType.compare("FAST") == 0)
     {
